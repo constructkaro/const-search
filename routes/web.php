bee80c1bb40520db12e03a4e76097e85c7760fdc
pratiksha187 <pratikshanmisal@gmail.com>
2026-03-31 10:25:21 +0530

    Add full project code

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ServiceAvailabilityController;
 
+Route::domain('vendor.constructkaro.in')->group(function () {
+    Route::get('/', function () {
+        return 'Hello from vendor.constructkaro.in';
+    });
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
