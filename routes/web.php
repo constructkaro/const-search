e683ffc22bf83b9d2e05423368f069e19a678640
pratiksha187 <pratikshanmisal@gmail.com>
2026-04-10 15:27:42 +0530

    chnages

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ use App\Http\Controllers\WeldingFabricationController;
 
 
 //vendor
-// Route::domain('vendor.constructkaro.in')->group(function () {
-    // Route::get('/', function () {
-    Route::get('/vendor', function () {
+Route::domain('vendor.constructkaro.in')->group(function () {
+    Route::get('/', function () {
+    // Route::get('/vendor', function () {
          return view('vendor.welcome');
     })->name('vendor');
 
@@ -94,7 +94,7 @@ use App\Http\Controllers\WeldingFabricationController;
 Route::get('/vendor/category/interior', [InteriorController::class, 'create'])->name('interior.create');
 
 Route::post('/interior/store', [InteriorController::class, 'store'])->name('interior.store');
-// });
+});
 
 
 // Route::get('/', [CustomerController::class, 'welcome'])->name('welcome');
