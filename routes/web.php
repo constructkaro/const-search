24c6a8c48aced6b4fa769972fa90f1f20617cb8e
pratiksha187 <pratikshanmisal@gmail.com>
2026-06-06 12:59:37 +0530

    chnages

--- routes/web.php
+++ routes/web.php
@@ -141,15 +141,15 @@ Route::middleware(['auth', 'role:super_admin,telecaller,admin'])
 
 
 // //vendor
-// Route::domain('vendor.constructkaro.com')->group(function () {
+Route::domain('vendor.constructkaro.com')->group(function () {
     // Route::get('/', function () {
     // Route::get('/vendor', function () {
     //      return view('vendor.welcome');
     // })->name('vendor');
 
-    // Route::get('/', [VendorController::class, 'welcome'])->name('vendor');
+    Route::get('/', [VendorController::class, 'welcome'])->name('vendor');
 
-    Route::get('/vendor', [VendorController::class, 'welcome'])->name('vendor');
+    // Route::get('/vendor', [VendorController::class, 'welcome'])->name('vendor');
 
     Route::get('/vendor/boq-form', [VendorBoqProfileController::class, 'create'])->name('vendor.boq.form');
     Route::post('/vendor/boq-form', [VendorBoqProfileController::class, 'store'])->name('vendor.boq.store');
@@ -237,7 +237,7 @@ Route::middleware(['auth', 'role:super_admin,telecaller,admin'])
     Route::post('/vendor/boq/accept-agreement', [VendorCategoryController::class, 'acceptBoqAgreement'])
     ->name('vendor.boq.acceptAgreement');
 // 
-// });
+});
 // 
 
 
